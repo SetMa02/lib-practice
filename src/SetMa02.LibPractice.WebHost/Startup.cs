cd953f3d30f1046919822953c21bdc0f616b5b94
Максим Майнагашев <74338834+SetMa02@users.noreply.github.com>
2021-06-29 10:38:49 +0700

    Fix initial PR with swagger
    Refs #4

--- src/SetMa02.LibPractice.WebHost/Startup.cs
+++ src/SetMa02.LibPractice.WebHost/Startup.cs
@@ -1,36 +1,27 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Mvc.ApplicationParts;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
-namespace SetMa02.libPractice
+namespace SetMa02.LibPractice.WebHost
 {
     public class Startup
     {
-        // This method gets called by the runtime. Use this method to add services to the container.
-        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
             services.AddSwaggerGen();
-
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             app.UseSwagger();
+
             app.UseSwaggerUI(c =>
             {
                 c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
             });
-            
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
@@ -40,7 +31,12 @@ namespace SetMa02.libPractice
 
             app.UseEndpoints(endpoints =>
             {
-                endpoints.MapGet("/", async context => { await context.Response.WriteAsync("Hello World!"); });
+                endpoints.MapGet(
+                    "/",
+                    async context => {
+                        await context.Response.WriteAsync("Hello World!");
+                    }
+                );
             });
         }
     }
